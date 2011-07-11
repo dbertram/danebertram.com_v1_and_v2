7ed2c7837310ea3e3b8e9e4252391b12508922db
Dane Bertram <dbertram@gmail.com>
2011-07-10 21:36:05 -0400

    initial commit

---
+++ old_website/calendar.php
@@ -0,0 +1,10 @@
+<?php
+
+	$_pageTitle = 'db.calendar';
+	$_includeDTree = false;
+	$_selectedLink = 6;
+	include_once('_header.php');
+
+?>
+		<iframe src="http://www.google.com/calendar/embed?showTitle=0&amp;showCalendars=0&amp;mode=WEEK&amp;height=450&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=dbertram%40gmail.com&amp;color=%232952A3&amp;src=2u5n8qu8po4rc5hjp06v5gumf8%40group.calendar.google.com&amp;color=%2329527A&amp;src=6njb91ie3q8mavn26mcoq06at0%40group.calendar.google.com&amp;color=%23A32929&amp;src=canadian__en%40holiday.calendar.google.com&amp;color=%23BE6D00&amp;src=usa__en%40holiday.calendar.google.com&amp;color=%2328754E&amp;src=p%23weather%40group.v.calendar.google.com&amp;color=%23B1365F&amp;ctz=America%2FEdmonton" style=" border-width:0 " width="684" height="450" frameborder="0" scrolling="no"></iframe>
+<?php include_once('_footer.php') ?>
